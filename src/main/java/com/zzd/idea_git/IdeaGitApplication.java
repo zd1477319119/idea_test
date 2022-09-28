5af9d6a022e1b7d93ef5cdaf27098ae83bbbf982
ZZD <1477319119@qq.com>
2022-09-28 10:02:18 +0800

    项目初始化

---
+++ src/main/java/com/zzd/idea_git/IdeaGitApplication.java
@@ -0,0 +1,13 @@
+package com.zzd.idea_git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeaGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdeaGitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zzd/idea_git/IdeaGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zzd.idea_git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeaGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
