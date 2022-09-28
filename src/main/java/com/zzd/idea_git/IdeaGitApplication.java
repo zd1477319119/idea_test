7b01586f7837ff60c290c69fa801c6d54abfc8d2
ZZD <1477319119@qq.com>
2022-09-28 10:48:04 +0800

    dev分支添加

--- src/main/java/com/zzd/idea_git/IdeaGitApplication.java
+++ src/main/java/com/zzd/idea_git/IdeaGitApplication.java
@@ -2,8 +2,10 @@ package com.zzd.idea_git;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Scope;
 
 @SpringBootApplication
+@Scope
 public class IdeaGitApplication {
 
     public static void main(String[] args) {
